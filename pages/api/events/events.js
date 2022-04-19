20c85e6684fde8ba7c45be8dbbd9a373cf16f559
Mahmoud <dooro.mah@gmail.com>
2022-04-19 18:24:32 +0000

    Use next api

--- pages/api/events/events.js
+++ pages/api/events/events.js
@@ -1,22 +1,25 @@
-export default (req, res) => {
-    res.status(200).json(
-        [
-            {
-                id: 1,
-                title: "Séminaire au centre de formation",
-                description: "Dans le cadre du lancement de la startup Ulteamcode, nous organisons une séminaire de formation en Excel",
-                Lieu: "Ranérou",
-                Date: "04/06/2022 09:30"
-            },
-            {
-                id: 2,
-                title: "Séminaire au centre de plyvalent",
-                description: "Dans le cadre du lancement de la startup Ulteamcode, nous organisons une séminaire de formation en Excel",
-                Lieu: "Ranérou",
-                Date: "08/06/2022 09:30"
-            }
-        ]
-    )
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../../utils/firebase'
+export default async(req, res) => {
+    const e = []
+    // 
+    switch (req.method) {
+        case 'GET':
+                const querySnapshot = await getDocs(collection(db, "events"));
+                querySnapshot.forEach((doc) => {
+                  e.push({
+                    id: doc.id,
+                    ...doc.data()
+                  })
+                });
+                res.status(200).json(e)
+            break;
+    
+        default:
+            break;
+    }
+      
+    
 }
 
 // export default allEvents    
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,39 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import {useEffect,useState} from 'react'
 import styles from '../styles/Home.module.css'
-import { collection, getDocs } from "firebase/firestore";
-import { db } from '../utils/firebase'
 import Link from 'next/link'
-
-
-
-export default function Home() {
-  const [events, setEvents] = useState([]);
-  async function SerializeEvents() {
-    const querySnapshot = await getDocs(collection(db, "events"));
-    const e = []
-    querySnapshot.forEach((doc) => {
-      e.push({
-        id: doc.id,
-        ...doc.data()
-      })
-    });
-    setEvents(e);
+export const getServerSideProps = async () => {
+  
+   const responses = await fetch('http://localhost:3000/api/events/events').then(data => {
+     return data.json()
+   }).catch(error => {
+     throw error
+   })
+   return {
+    props: {
+      events: responses
+    }
   }
+
+   
+}
+
+export default function Home({events}) {
+
+  // const [events, setEvents] = useState([]);
+  // async function SerializeEvents() {
+  //   const querySnapshot = await getDocs(collection(db, "events"));
+  //   const e = []
+  //   querySnapshot.forEach((doc) => {
+  //     e.push({
+  //       id: doc.id,
+  //       ...doc.data()
+  //     })
+  //   });
+  //   setEvents(e);
+  // }
   useEffect(() => {
-    SerializeEvents()
+    console.log(events);
   }, []);
   return (
     <div className={styles.container}>
@@ -31,23 +42,22 @@ export default function Home() {
         <meta name="description" content="L'une des meilleurs canaux de la publicité" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
         <h1 className={styles.title}>
           Bienvenue sur <a href="/">ULTeam events</a>
         </h1>
             {
                 events.map(event => {
                   // Sans le return le contenue du ul ne sera pas visible
-                 return (<ul key={event.title}>
-                    <li className={styles.title}><Link href={`http://localhost:3000/events/`+ event.id}> {event.title}</Link></li>
-                    <li>{event.description}</li>
-                    <li>{event.location}</li>
-                    <li>{event.moment}</li>
-                  </ul>) 
+                 return (<Link href={`http://localhost:3000/events/`+ event.id}>
+                                <ul key={event.id}>
+                                              <li className={styles.title}> {event.title}</li>
+                                              <li>{event.description}</li>
+                                              <li>{event.location}</li>
+                                              <li>{event.moment}</li>
+                                </ul>
+                                </Link>) 
                 })
               }
-      </main>
       
     </div>
   )
